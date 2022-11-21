9f921df2fd4aecdec6211ec07373930ca1da71d3
shamisashams <shamisaashams@gmail.com>
2022-11-21 13:15:34 +0400

    some bugs - district - filters

--- resources/js/components/HeroSlider.jsx
+++ resources/js/components/HeroSlider.jsx
@@ -5,20 +5,18 @@ import "swiper/css/navigation";
 import { Navigation } from "swiper";
 import { heroSlider } from "./Data";
 //import { Link } from "react-router-dom";
-import { Link, usePage } from '@inertiajs/inertia-react'
+import { Link, usePage } from "@inertiajs/inertia-react";
 import { HiArrowRight, HiArrowLeft } from "react-icons/hi";
 import Filters from "./Filters";
 import { CiSearch } from "react-icons/ci";
 import { Inertia } from "@inertiajs/inertia";
 
 const HeroSlider = () => {
-
-    const {sliders,localizations} = usePage().props;
+    const { sliders, localizations } = usePage().props;
 
     //console.log(sliders)
-  const prevRef = useRef(null);
-  const nextRef = useRef(null);
-
+    const prevRef = useRef(null);
+    const nextRef = useRef(null);
 
     let appliedFilters = {};
     let urlParams = new URLSearchParams(window.location.search);
@@ -27,14 +25,14 @@ const HeroSlider = () => {
         appliedFilters[index] = value.split(",");
     });
 
-    function search(){
+    function search() {
         let params = [];
 
         for (let key in appliedFilters) {
             params.push(key + "=" + appliedFilters[key].join(","));
         }
 
-        Inertia.visit(route('client.apartment.index') + "?" + params.join("&"));
+        Inertia.visit(route("client.apartment.index") + "?" + params.join("&"));
     }
 
     const renderHTML = (rawHTML) =>
@@ -42,111 +40,135 @@ const HeroSlider = () => {
             dangerouslySetInnerHTML: { __html: rawHTML },
         });
 
+    return (
+        <div className="relative">
+            <Swiper
+                navigation={true}
+                modules={[Navigation]}
+                className="heroSlider"
+                onInit={(swiper) => {
+                    swiper.params.navigation.prevEl = prevRef.current;
+                    swiper.params.navigation.nextEl = nextRef.current;
+                    swiper.navigation.init();
+                    swiper.navigation.update();
+                }}
+            >
+                {sliders.map((item, index) => {
+                    let flats = 0;
 
+                    if (item.project) {
+                        if (item.project.apartments) {
+                            item.project.apartments.map((apartment, n) => {
+                                flats += apartment.floors.length;
+                            });
+                        }
+                    }
 
-  return (
-    <div className="relative">
-      <Swiper
-        navigation={true}
-        modules={[Navigation]}
-        className="heroSlider"
-        onInit={(swiper) => {
-          swiper.params.navigation.prevEl = prevRef.current;
-          swiper.params.navigation.nextEl = nextRef.current;
-          swiper.navigation.init();
-          swiper.navigation.update();
-        }}
-      >
-        {sliders.map((item, index) => {
-
-            let flats = 0;
-
-            if (item.project){
-                if (item.project.apartments){
-                    item.project.apartments.map((apartment,n) => {
-                        flats += apartment.floors.length;
-                    })
-                }
-            }
-
-          return (
-            <SwiperSlide key={index} className="!h-auto">
-              <div className="xl:pt-20 pt-14 pb-10">
-                <img
-                  className="absolute left-0 top-0 w-full h-full object-cover -z-10"
-                  src={item.file?item.file.full_url:null}
-                  alt=""
-                />
-                <div className="wrapper">
-                  <div className="max-w-2xl">
-                    <div className="xl:text-5xl sm:text-3xl text-xl bold xl:mb-8 mb-3">
-                      {item.title}
-                    </div>
-                    {renderHTML(item.description)}
-                    <Link
-                      className="group flex items-center justify-center bg-white rounded-md w-fit p-3 bold sm:mt-8 mt-4"
-                      href="/"
+                    return (
+                        <SwiperSlide key={index} className="!h-auto">
+                            <div className="xl:pt-20 pt-14 pb-10">
+                                <img
+                                    className="absolute left-0 top-0 w-full h-full object-cover -z-10"
+                                    src={item.file ? item.file.full_url : null}
+                                    alt=""
+                                />
+                                <div className="wrapper">
+                                    <div className="max-w-2xl">
+                                        <div className="xl:text-5xl sm:text-3xl text-xl bold xl:mb-8 mb-3">
+                                            {item.title}
+                                        </div>
+                                        {renderHTML(item.description)}
+                                        <Link
+                                            className="group flex items-center justify-center bg-white rounded-md w-fit p-3 bold sm:mt-8 mt-4"
+                                            href="/"
+                                        >
+                                            <span className="group-hover:max-w-sm max-w-0 overflow-hidden whitespace-nowrap group-hover:pr-2 transition-all duration-300">
+                                                {__(
+                                                    "client.view_more",
+                                                    localizations
+                                                )}
+                                            </span>
+                                            <HiArrowRight className="w-6 h-6" />
+                                        </Link>
+                                    </div>
+                                    {item.project ? (
+                                        <div className="lg:flex hidden justify-start xl:mt-60 mt-10 ">
+                                            <div className="flex mr-10">
+                                                <div className="text-right opacity-50 pr-3 border-r border-slate-500">
+                                                    {renderHTML(
+                                                        __(
+                                                            "client.slide_recreational_space",
+                                                            localizations
+                                                        ).newLineToBr()
+                                                    )}
+                                                </div>
+                                                <span className="bold text-3xl pl-3 align-middle">
+                                                    {
+                                                        item.project
+                                                            .recreational_space
+                                                    }{" "}
+                                                    კვ.მ
+                                                </span>
+                                            </div>
+                                            <div className="flex mr-10">
+                                                <div className="text-right opacity-50 pr-3 border-r  border-slate-500">
+                                                    {renderHTML(
+                                                        __(
+                                                            "client.slide_parking",
+                                                            localizations
+                                                        ).newLineToBr()
+                                                    )}
+                                                </div>
+                                                <span className="bold text-3xl pl-3 align-middle">
+                                                    {item.project.parking}
+                                                </span>
+                                            </div>
+                                            <div className="flex mr-10">
+                                                <div className="text-right opacity-50 pr-3 border-r  border-slate-500">
+                                                    {renderHTML(
+                                                        __(
+                                                            "client.slide_available_flats",
+                                                            localizations
+                                                        ).newLineToBr()
+                                                    )}
+                                                </div>
+                                                <span className="bold text-3xl pl-3 align-middle">
+                                                    {flats}
+                                                </span>
+                                            </div>
+                                        </div>
+                                    ) : null}
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                    );
+                })}
+                <div className="absolute left-1/2 xl:bottom-10 xl:top-auto bottom-auto top-3 -translate-x-1/2 wrapper flex justify-end items-center z-20">
+                    <button
+                        ref={prevRef}
+                        className="lg:w-12 lg:h-12 w-8 h-8 flex items-center justify-center bg-white rounded-md"
                     >
-                      <span className="group-hover:max-w-sm max-w-0 overflow-hidden whitespace-nowrap group-hover:pr-2 transition-all duration-300">
-                        {__('client.view_more',localizations)}
-                      </span>
-                      <HiArrowRight className="w-6 h-6" />
-                    </Link>
-                  </div>
-                    {item.project ?<div className="lg:flex hidden justify-start xl:mt-60 mt-10 ">
-                    <div className="flex mr-10">
-                      <div className="text-right opacity-50 pr-3 border-r border-slate-500">
-                          {renderHTML(__('client.slide_recreational_space',localizations).newLineToBr())}
-                      </div>
-                      <span className="bold text-3xl pl-3 align-middle">
-                        {item.project.recreational_space} კვ.მ
-                      </span>
-                    </div>
-                    <div className="flex mr-10">
-                      <div className="text-right opacity-50 pr-3 border-r  border-slate-500">
-                          {renderHTML(__('client.slide_parking',localizations).newLineToBr())}
-                      </div>
-                      <span className="bold text-3xl pl-3 align-middle">
-                        {item.project.parking}
-                      </span>
-                    </div>
-                    <div className="flex mr-10">
-                      <div className="text-right opacity-50 pr-3 border-r  border-slate-500">
-                          {renderHTML(__('client.slide_available_flats',localizations).newLineToBr())}
-                      </div>
-                      <span className="bold text-3xl pl-3 align-middle">
-                        {flats}
-                      </span>
-                    </div>
-                  </div>:null}
+                        <HiArrowLeft className="w-6 h-6" />
+                    </button>
+                    <button
+                        ref={nextRef}
+                        className="lg:w-12 lg:h-12 w-8 h-8 flex items-center justify-center bg-white rounded-md  xl:ml-6 ml-4"
+                    >
+                        <HiArrowRight className="w-6 h-6" />
+                    </button>
                 </div>
-              </div>
-            </SwiperSlide>
-          );
-        })}
-        <div className="absolute left-1/2 xl:bottom-10 xl:top-auto bottom-auto top-3 -translate-x-1/2 wrapper flex justify-end items-center z-20">
-          <button
-            ref={prevRef}
-            className="lg:w-12 lg:h-12 w-8 h-8 flex items-center justify-center bg-white rounded-md"
-          >
-            <HiArrowLeft className="w-6 h-6" />
-          </button>
-          <button
-            ref={nextRef}
-            className="lg:w-12 lg:h-12 w-8 h-8 flex items-center justify-center bg-white rounded-md  xl:ml-6 ml-4"
-          >
-            <HiArrowRight className="w-6 h-6" />
-          </button>
+            </Swiper>
+            <div className="xl:absolute xl:bottom-40 xl:left-1/2 xl:-translate-x-1/2 flex xl:flex-row items-start h-fit xl:w-fit w-auto flex-col wrapper z-20 max-w-7xl bg-white xl:py-6 py-12 xl:px-2 rounded-3xl xl:shadow-lg  ">
+                <Filters appliedFilters={appliedFilters} />
+                <button
+                    onClick={search}
+                    className="xl:w-14 w-full h-14 flex items-center justify-center bg-custom-blue text-white rounded-md text-3xl md:ml-5 shrink-0"
+                >
+                    <CiSearch />
+                </button>
+            </div>
         </div>
-      </Swiper>
-      <div className="xl:absolute xl:bottom-40 xl:left-1/2 xl:-translate-x-1/2 flex xl:flex-row flex-col wrapper z-20 max-w-7xl bg-white xl:py-6 py-12 xl:px-2 rounded-3xl xl:shadow-lg  ">
-        <Filters appliedFilters={appliedFilters} />
-        <button onClick={search} className="xl:w-14 h-14 flex items-center justify-center bg-custom-blue text-white rounded-md text-3xl md:ml-5 shrink-0">
-          <CiSearch />
-        </button>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default HeroSlider;
--- resources/js/components/Shared.jsx
+++ resources/js/components/Shared.jsx
@@ -1,79 +1,82 @@
 import React from "react";
 import { useState } from "react";
 import { BiChevronDown } from "react-icons/bi";
-import {Link} from "@inertiajs/inertia-react";
+import { Link } from "@inertiajs/inertia-react";
 //import CalIcon from "../assets/images/icons/10.png";
-import {usePage } from '@inertiajs/inertia-react';
+import { usePage } from "@inertiajs/inertia-react";
 
 export const IconDiv = ({ img }) => {
-  return (
-    <div className="flex justify-center items-center bg-white rounded-xl w-12 h-12 shrink-0 shadow">
-      <img src={img} alt="" />
-    </div>
-  );
+    return (
+        <div className="flex justify-center items-center bg-white rounded-xl w-12 h-12 shrink-0 shadow">
+            <img src={img} alt="" />
+        </div>
+    );
 };
 
 export const SelectFilter = ({ icon, title, subtitle, children }) => {
-  const [clicked, setClicked] = useState(false);
-  return (
-    <div className="select relative xl:border-r xl:px-5 md:px-3 mx-1  xl:mb-0 mb-2 2xl:text-base text-sm">
-      <div
-        onClick={() => setClicked(!clicked)}
-        className="flex items-start justify-start cursor-pointer xl:mb-0 mb-3"
-      >
-        <div className="flex justify-center items-center bg-custom-slate-200 rounded-xl w-12 h-12 shrink-0">
-          {icon}
+    const [clicked, setClicked] = useState(false);
+    return (
+        <div className="select relative xl:border-r xl:px-5 md:px-3 mx-1  xl:mb-0 mb-2 2xl:text-base text-sm">
+            <div
+                onClick={() => setClicked(!clicked)}
+                className="flex items-start justify-start cursor-pointer xl:mb-0 mb-3"
+            >
+                <div className="flex justify-center items-center bg-custom-slate-200 rounded-xl w-12 h-12 shrink-0">
+                    {icon}
+                </div>
+                <div className="mx-3 whitespace-nowrap">
+                    {title}
+                    <br />
+                    <span className="opacity-30 text-sm">{subtitle}</span>
+                </div>
+                <BiChevronDown
+                    className={`w-5 h-5 transition-all duration-300 ${
+                        clicked ? "rotate-180" : ""
+                    }`}
+                />
+            </div>
+            <div
+                className={`xl:absolute xl:left-0 xl:top-24 rounded-xl bg-white xl:shadow-lg transition-all duration-300 w-full text-sm z-40  ${
+                    clicked
+                        ? "opacity-100 visible translate-y-0 max-h-96"
+                        : "xl:opacity-0 xl:invisible xl:translate-y-10 max-h-0"
+                }`}
+            >
+                {children}
+            </div>
         </div>
-        <div className="mx-3 whitespace-nowrap">
-          {title}
-          <br />
-          <span className="opacity-30 text-sm">{subtitle}</span>
-        </div>
-        <BiChevronDown
-          className={`w-5 h-5 transition-all duration-300 ${
-            clicked ? "rotate-180" : ""
-          }`}
-        />
-      </div>
-      <div
-        className={`xl:absolute xl:left-0 xl:top-24 rounded-xl bg-white xl:shadow-lg transition-all duration-300 xl:py-4 xl:px-6 w-full text-sm z-40 overflow-hidden ${
-          clicked
-            ? "opacity-100 visible translate-y-0 max-h-96"
-            : "xl:opacity-0 xl:invisible xl:translate-y-10 max-h-0"
-        }`}
-      >
-        {children}
-      </div>
-    </div>
-  );
+    );
 };
 
 export const MainButton = ({ text, onClick }) => {
-  return (
-    <button onClick={onClick} className="bg-custom-blue shadow-lg shadow-custom-blue/[0.5] text-white py-3 px-8 rounded-lg">
-      {text}
-    </button>
-  );
+    return (
+        <button
+            onClick={onClick}
+            className="bg-custom-blue shadow-lg shadow-custom-blue/[0.5] text-white py-3 px-8 rounded-lg"
+        >
+            {text}
+        </button>
+    );
 };
 
 export const CallButton = ({ onClick }) => {
-    const {localizations} = usePage().props;
-  return (
-    <button
-      onClick={onClick}
-      className="relative bg-custom-blue shadow-lg shadow-custom-blue/[0.5] text-white py-3 px-10 rounded-lg ml-5 my-5"
-    >
-      <img
-        className="absolute -left-10 top-1/2 -translate-y-1/2 "
-        src="/client/assets/images/icons/10.png"
-        alt=""
-      />
-        {__('client.call_request',localizations)}
-    </button>
-  );
+    const { localizations } = usePage().props;
+    return (
+        <button
+            onClick={onClick}
+            className="relative bg-custom-blue shadow-lg shadow-custom-blue/[0.5] text-white py-3 px-10 rounded-lg ml-5 my-5"
+        >
+            <img
+                className="absolute -left-10 top-1/2 -translate-y-1/2 "
+                src="/client/assets/images/icons/10.png"
+                alt=""
+            />
+            {__("client.call_request", localizations)}
+        </button>
+    );
 };
 
-export const Pagination = ({data}) => {
+export const Pagination = ({ data }) => {
     let links = function (links) {
         let rows = [];
         //links.shift();
@@ -84,11 +87,7 @@ export const Pagination = ({data}) => {
                     rows.push(
                         <Link
                             href={item.url}
-                            className={
-                                item.active
-                                    ? "mx-3"
-                                    : "mx-3 opacity-50"
-                            }
+                            className={item.active ? "mx-3" : "mx-3 opacity-50"}
                         >
                             {item.label}
                         </Link>
@@ -127,13 +126,13 @@ export const Pagination = ({data}) => {
             </Link>
         ) : null;
     };
-  return (
-    <div className="flex justify-center py-10">
-      {/*<button className="mx-3 ">1</button>
+    return (
+        <div className="flex justify-center py-10">
+            {/*<button className="mx-3 ">1</button>
       <button className="mx-3 opacity-50">2</button>
       <button className="mx-3 opacity-50">3</button>
       <button className="mx-3 opacity-50">4</button>*/}
-        {links(data)}
-    </div>
-  );
+            {links(data)}
+        </div>
+    );
 };
